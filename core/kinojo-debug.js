32974c5f78678b5b6acfa621d9917180324ae5aa
losnah513 <losnah513@gmail.com>
2026-06-26 13:36:29 +0900

    Delete core directory

--- core/kinojo-debug.js
+++
@@ -1,7 +0,0 @@
-/* KINOJO DEBUG ENGINE - 관리자 전용 진단 토글 */
-(function(){
-  'use strict';
-  const enabled = /[?&]debug=1(&|$)/.test(location.search || '');
-  function log(){ if(enabled) console.debug.apply(console, ['[KINOJO DEBUG]'].concat(Array.from(arguments))); }
-  window.KinojoDebug = { version:'1.3.1.17', enabled, log };
-})();
