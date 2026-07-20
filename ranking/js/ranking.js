9a1177d3ba3e350375e3c048ca670642c34452ed
losnah513 <losnah513@gmail.com>
2026-07-20 11:04:27 +0900

    Delete ranking directory

--- ranking/js/ranking.js
+++
@@ -1,9 +0,0 @@
-/*
- * Deprecated in STEP 6.
- * 레기온 전체 순위는 ranking-utils/data/card/render/events/app.js 모듈을 사용합니다.
- * 이 파일은 구 경로 진단용으로만 남겨두며 페이지에서는 로드하지 않습니다.
- */
-(function(){
-  'use strict';
-  window.KinojoRankingLegacyRemoved = true;
-})();
