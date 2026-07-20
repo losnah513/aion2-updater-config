a646da6b11d66352702861ae72d16f67d329d13a
losnah513 <losnah513@gmail.com>
2026-07-20 11:04:48 +0900

    Delete ui directory

--- ui/kinojo-admin-panel.js
+++
@@ -1,9 +0,0 @@
-/*
- * KINOJO Common Admin Panel retired 2026-07-17.
- * 운영 기능은 /admin/ 및 /m/admin/ 콘솔로 통합되었습니다.
- * 기존 페이지의 script 참조 호환을 위해 no-op API만 유지합니다.
- */
-(function(){
-  'use strict';
-  window.KinojoAdminPanel={bind:function(){}};
-})();
