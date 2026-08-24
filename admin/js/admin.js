294c79368923a8b1514bd5c60853553dc3aceae2
losnah513 <losnah513@gmail.com>
2026-08-24 11:30:23 +0900

    Keep existing admin loader cache contract

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,4 +1,4 @@
-/* KINOJO Admin modular loader v2026082401 */
+/* KINOJO Admin modular loader v2026082202 */
 (function(){
   'use strict';
   const current=document.currentScript;
@@ -20,7 +20,7 @@
   function loadScript(name){
     return new Promise((resolve,reject)=>{
       const script=document.createElement('script');
-      script.src=new URL(name+'?cache=2026082401',base).href;
+      script.src=new URL(name+'?cache=2026082202',base).href;
       script.async=false;
       script.onload=resolve;
       script.onerror=()=>reject(new Error('관리자 모듈을 불러오지 못했습니다: '+name));
